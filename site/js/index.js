0785d23a42617ca38eb691c2cb7ca908c154b73e
GustavoMD <gustavomilcharekdavila@gmail.com>
2020-02-16 01:05:06 -0300

    styles and js files

--- site/js/index.js
+++ site/js/index.js
@@ -1,3 +1,50 @@
 /**
  * This will contain the application interactive behavior.
  */
+// global variables
+
+let inputs = document.querySelectorAll('.form-input');
+let alertEmptyField = document.querySelectorAll('.empty-field-alert')
+let form = document.querySelector('.form-wrapper > form');
+let email = document.querySelector('#email');
+
+email.addEventListener('keypress', function() {
+    email.classList.remove('red-text');
+});
+
+form.addEventListener('submit', verifyData);
+
+function verifyDatsa() {
+    alert('teste');
+}
+
+
+function verifyData(e) {
+    let verify = true
+    inputs.forEach((input, i) => {
+        if (input.value == '') {
+            e.preventDefault();
+            alertEmptyField[i].style.display = 'block';
+            alertEmptyField[i].textContent = input.placeholder + ' cannot be empty'
+            verify = false;
+        } else {
+            alertEmptyField[i].style.display = 'none';
+        }
+        if (input.value != '' && input.id == 'email' && !validateEmail(input.value)) {
+            input.classList.add('red-text');
+            alertEmptyField[i].style.display = 'block';
+            alertEmptyField[i].textContent = 'Looks like this is not an email'
+            verify = false;
+        } else {
+            input.classList.remove('red-text');
+        }
+    });
+    if (verify) {
+        alert('Your free trial was claimed');
+    }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
